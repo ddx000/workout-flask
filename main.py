39d9fe080ea16fe4d9298030df838d40516a83f4
ddx000 <35722473+ddx000@users.noreply.github.com>
2021-07-10 21:42:32 +0800

    first coomit

---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import render_template
+
+GOOGLE_OAUTH2_CLIENT_ID = '這部分輸入自己的_oauth2_client_id'
+
+print("hefewf")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', google_oauth2_client_id=GOOGLE_OAUTH2_CLIENT_ID)
+
+
+@app.route('/google_sign_in', methods=['POST'])
+def google_sign_in():
+    token = request.json['id_token']
+
+    try:
+        id_info = id_token.verify_oauth2_token(
+            token,
+            requests.Request(),
+            GOOGLE_OAUTH2_CLIENT_ID
+        )
+        if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+    except ValueError:
+        # Invalid token
+        raise ValueError('Invalid token')
+
+    print('登入成功')
+    return jsonify({}), 200
+
+
+app.run()
